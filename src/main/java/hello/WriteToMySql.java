7e6d18361eb813a64115ca3d7443b3621fe5376d
Martin <martinparre94@gmail.com>
2016-11-18 16:46:22 +0100

    Scrittura di un utente nella tabella Utenti in zquiz phpmyadmin

--- src/main/java/hello/WriteToMySql.java
+++ src/main/java/hello/WriteToMySql.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import com.mysql.jdbc.PreparedStatement;
+
 public class WriteToMySql {
 
 	public static void connection() {
@@ -16,7 +18,7 @@ public class WriteToMySql {
 		}
 	}
 	
-	public static void ConnectionToMySql(){
+	public static void ConnectionToMySql(String Username, String Password, String Nome, String Cognome){
 		
 		connection();
 		String host = "jdbc:mysql://localhost/zquiz";
@@ -24,6 +26,14 @@ public class WriteToMySql {
 		String password = "";
 		try {
 			Connection connect = DriverManager.getConnection(host, username, password);
+			PreparedStatement statement = (PreparedStatement) connect.prepareStatement("INSERT INTO utente(Username,Password,Nome,Cognome) VALUES(?,?,?,?)");
+			statement.setString(1, Username);
+			statement.setString(2, Password);
+			statement.setString(3, Nome);
+			statement.setString(4, Cognome);
+			statement.executeUpdate();
+			statement.close();
+			connect.close();
 			System.out.println("Works :)");
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -33,7 +43,7 @@ public class WriteToMySql {
 	public static void main(String[] args) {
 		
 		//connection(); prima prova
-		ConnectionToMySql();
+		ConnectionToMySql("Martinparre","juventus","Martin","Cossali");
 		
 	}
 
