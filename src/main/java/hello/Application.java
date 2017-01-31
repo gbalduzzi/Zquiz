7e78c6800ca89105ed3bb771a3fc1c644b28d007
Martin <martinparre94@gmail.com>
2017-01-31 09:36:23 +0100

    aggiunto metodo che azzera tutti i match attivi all'avvio

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -3,6 +3,8 @@ package hello;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import database.DBQueries;
+
 import java.lang.Thread;
 
 @SpringBootApplication
@@ -10,6 +12,8 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+        
+        DBQueries.EndAllMatch();
         
         //inizio thread della gestione della coda.
         QueueController gestoreCoda= new QueueController();
